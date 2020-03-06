55703355c788aa35978b34bd8010033a2d52364e
minkyung <mnk918p@gmail.com>
2020-03-06 17:46:33 +0900

    study

---
+++ index.js
@@ -0,0 +1,11 @@
+// 자바스크립트를 쓰는 방법
+// 1. Create 
+// 2. Initialize
+// let은 create나 initialize를 할 때에만 사용
+// 3. Use
+
+
+// var : let과 const가 없었을 때 쓰이던 것, 지금은 let으로 써주는 것이 좋다.
+// let : 바꿀 수 있는 변수
+// const : (상수) 바꿀 수 없는 변수
+console.log("I am working");
