719b63b3519b7af748482faa9f05fa281ed30547
minkyung <mnk918p@gmail.com>
2020-03-31 14:15:32 +0900

    the end of todo js

--- weather.js
+++ weather.js
@@ -1,6 +1,23 @@
-const API_KEY = "293fa0ccfd84823b65894617b3cb7a03";
+const weather = document.querySelector(".js-weather") ;
 
+const API_KEY = "293fa0ccfd84823b65894617b3cb7a03";
 const COORDS = 'coords';
+
+// 새로고침 없이 데이터를 계속 가져오자!
+function getWeather(lat, lng) {
+    // 데이터를 가져올 때에는 fetch를 쓴다.
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+        ).then(function(response) {
+            // fetch가 완료되고 난 뒤에 실행될 함수를 여기에 넣으면 된당!
+            return response.json()
+        }).then(function(json){
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperature}도씨 @ ${place}`;
+        });
+    // units format -> format을 바꿔 줄 수 있떵!
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -17,6 +34,7 @@ function handleGeoSuccess(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -33,6 +51,9 @@ function loadCoords(){
         askForCoords();
     } else {
         // getweather
+        const parseCoords = JSON.parse(loadedCoords);
+        console.log(parseCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
